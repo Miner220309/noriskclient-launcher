fb5ad0a3e4dc6807801c4e144c886fc58ac4f8a1
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-01-03 23:20:40 +0100

    Added Correct Java Arguments but Still not Working

--- src/App.tsx
+++ src/App.tsx
@@ -7,44 +7,45 @@ import {blue, cyan} from "@material-ui/core/colors";
 import {LauncherProfile} from "./components/interfaces/LauncherAccount";
 import {promisified} from "tauri/api/tauri";
 import {readTextFile} from "tauri/api/fs";
+import NoRiskBackground from "./images/main_bg.jpg"
 
 const theme = createMuiTheme({
-    palette: {
-        primary: blue,
-        secondary: cyan,
-    },
+  palette: {
+    primary: blue,
+    secondary: cyan,
+  },
 });
 
 export const App = () => {
-    const [profile, setProfile] = useState<LauncherProfile>({} as LauncherProfile);
-    const [profiles, setProfiles] = useState<Array<LauncherProfile>>([]);
+  const [profile, setProfile] = useState<LauncherProfile>({} as LauncherProfile);
+  const [profiles, setProfiles] = useState<Array<LauncherProfile>>([]);
 
-    const switchProfile = () => {
-    }
+  const switchProfile = () => {
+  }
 
-    useEffect(() => {
-        const fetchUserProfiles = async () => {
-            const mcDir = await promisified({cmd: "minecraftDir"});
-            const profiles = await readTextFile(`${mcDir}/launcher_accounts.json`);
-            const profilesData = await JSON.parse(profiles);
-            setProfiles(await Object.entries(profilesData.accounts).map(value => {
-                if (value[0] === profilesData.activeAccountLocalId) {
-                    setProfile(value[1] as LauncherProfile);
-                }
-                return value[1] as LauncherProfile;
-            }))
+  useEffect(() => {
+    const fetchUserProfiles = async () => {
+      const mcDir = await promisified({cmd: "minecraftDir"});
+      const profiles = await readTextFile(`${mcDir}/launcher_accounts.json`);
+      const profilesData = await JSON.parse(profiles);
+      setProfiles(await Object.entries(profilesData.accounts).map(value => {
+        if (value[0] === profilesData.activeAccountLocalId) {
+          setProfile(value[1] as LauncherProfile);
         }
-        fetchUserProfiles();
-    }, []);
+        return value[1] as LauncherProfile;
+      }))
+    }
+    fetchUserProfiles();
+  }, []);
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Box bgcolor={cyan} height={"100%"}>
-                <NavBar profile={profile} setProfile={setProfile}/>
-                <Box height={"auto"}>
-                    <StartButton profile={profile}/>
-                </Box>
-            </Box>
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider theme={theme}>
+      <Box style={{backgroundImage: `url(${NoRiskBackground})`, backgroundPosition: "center"}} height={"100%"}>
+        <NavBar profile={profile} setProfile={setProfile}/>
+        <Box height={"auto"}>
+          <StartButton profile={profile}/>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  )
 }
