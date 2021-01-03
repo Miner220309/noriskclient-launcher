d8142431f931c70514e356a4a87f4b05c865e014
F0X <f0xde@protonmail.com>
2021-01-03 12:22:18 +0100

    Get default `.minecraft` dir with command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,9 @@
 
 mod cmd;
 
+use cmd::Cmd;
+use std::env;
+
 #[cfg(target_os = "windows")]
 use winapi::um::shellscalingapi::{SetProcessDpiAwareness, PROCESS_PER_MONITOR_DPI_AWARE};
 
@@ -15,21 +18,29 @@ fn main() {
     }
 
     tauri::AppBuilder::new()
-        .invoke_handler(|_webview, arg| {
-            use cmd::Cmd::*;
-            match serde_json::from_str(arg) {
-                Err(e) => Err(e.to_string()),
-                Ok(command) => {
-                    match command {
-                        // definitions for your custom commands from Cmd here
-                        MyCustomCommand { argument } => {
-                            //  your command code
-                            println!("{}", argument);
-                        }
-                    }
-                    Ok(())
+        .invoke_handler(|webview, arg| {
+            match serde_json::from_str(arg).map_err(|e| e.to_string())? {
+                Cmd::MinecraftDir { callback, error } => {
+                    tauri::execute_promise(
+                        webview,
+                        || {
+                            Ok(if cfg!(target_os = "windows") {
+                                format!(r"{}\.minecraft", env::var("APPDATA")?)
+                            } else if cfg!(target_os = "macos") {
+                                format!(
+                                    "{}/Library/Application Support/minecraft",
+                                    env::var("HOME")?
+                                )
+                            } else {
+                                format!("{}/.minecraft", env::var("HOME")?)
+                            })
+                        },
+                        callback,
+                        error,
+                    );
                 }
             }
+            Ok(())
         })
         .build()
         .run();
--- src/components/action/StartButton.tsx
+++ src/components/action/StartButton.tsx
@@ -1,36 +1,42 @@
 import React from "react";
-import {Button, createStyles, makeStyles, Theme, useTheme} from "@material-ui/core";
-import {BaseDirectory, readDir, readTextFile} from "tauri/api/fs";
-import {PlayArrow} from "@material-ui/icons";
+import {
+  Button,
+  createStyles,
+  makeStyles,
+  Theme,
+  useTheme,
+} from "@material-ui/core";
+import { readTextFile } from "tauri/api/fs";
+import { PlayArrow } from "@material-ui/icons";
+import { promisified } from "tauri/api/tauri";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        button: {
-            margin: theme.spacing(1),
-            position: "absolute",
-            bottom: 0,
-        },
-    }),
+  createStyles({
+    button: {
+      margin: theme.spacing(1),
+      position: "absolute",
+      bottom: 0,
+    },
+  })
 );
 
 export const StartButton = () => {
-    const theme = useTheme();
-    const classes = useStyles(theme);
-    return (
-        <Button
-            variant="contained"
-            color="primary"
-            className={classes.button}
-            onClick={() => {
-                console.log(BaseDirectory.App)
-                readTextFile("AppData/Roaming/.minecraft/launcher_profiles.json",{dir: BaseDirectory.Home}).then(value => {
-                    console.log(value)
-                }).catch(reason => {
-                    console.log(reason)
-                })
-            }}
-            startIcon={<PlayArrow/>}>
-            Start
-        </Button>
-    )
-}
+  const theme = useTheme();
+  const classes = useStyles(theme);
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      className={classes.button}
+      onClick={async () => {
+        const mcDir = await promisified({ cmd: "minecraftDir" });
+        const profiles = await readTextFile(`${mcDir}/launcher_profiles.json`);
+        console.log(profiles);
+      }}
+      startIcon={<PlayArrow />}
+    >
+      Start
+    </Button>
+  );
+};
+
